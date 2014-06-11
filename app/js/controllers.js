32d98d2f7143b6c60ca6f8ddb3805df186cbb547
Yuriy Dybskiy <yuriy@dybskiy.com>
2014-06-10 23:38:48 -0700

    Add codemirror

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,7 +4,12 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
-
+    $scope.editorOptions = {
+        lineWrapping : true,
+        lineNumbers: true,
+        // readOnly: 'nocursor',
+        mode: 'raml',
+    };
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
