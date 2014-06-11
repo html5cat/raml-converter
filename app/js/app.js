899a7553bc5249a0dc5b689898d1651ce46f898a
Yuriy Dybskiy <yuriy@dybskiy.com>
2014-06-11 16:17:04 -0700

    Make api-designer work and add two panes

--- app/js/app.js
+++ app/js/app.js
@@ -8,7 +8,8 @@ angular.module('myApp', [
   'myApp.services',
   'myApp.directives',
   'myApp.controllers',
-  'ui.codemirror'
+  'ui.codemirror',
+  'ramlEditorApp'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
@@ -16,62 +17,65 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
 
-angular.module('ramlEditorApp')
-      .factory('MyFileSystem', function ($q, config, eventService) {
-        var service = {};
+// angular.module('ramlEditorApp')
+//       .factory('MyFileSystem', function ($q, config, eventService) {
+//         var service = {};
 
-        service.directory = function (path) {
-          var deferred = $q.defer();
+//         service.directory = function (path) {
+//           var deferred = $q.defer();
 
-          // Your magic goes here:
-           // Do deferred.resolve(data); to fulfull the promise or
-           // deferred.reject(error); to reject it.
+//           // Your magic goes here:
+//            // Do deferred.resolve(data); to fulfull the promise or
+//            // deferred.reject(error); to reject it.
 
-           return deferred.promise;
-        };
+//            return deferred.promise;
+//         };
 
-        service.load = function (path, name) {
-          var deferred = $q.defer();
+//         service.load = function (path, name) {
+//           var deferred = $q.defer();
 
-          // Your magic goes here:
-           // Do deferred.resolve(data); to fulfull the promise or
-           // deferred.reject(error); to reject it.
+//           // Your magic goes here:
+//            // Do deferred.resolve(data); to fulfull the promise or
+//            // deferred.reject(error); to reject it.
 
-           return deferred.promise;
-        };
+//            return deferred.promise;
+//         };
 
-        service.remove = function (path, name) {
-          var deferred = $q.defer();
+//         service.remove = function (path, name) {
+//           var deferred = $q.defer();
 
-          // Your magic goes here:
-           // Do deferred.resolve(data); to fulfull the promise or
-           // deferred.reject(error); to reject it.
+//           // Your magic goes here:
+//            // Do deferred.resolve(data); to fulfull the promise or
+//            // deferred.reject(error); to reject it.
 
-           return deferred.promise;
-        };
+//            return deferred.promise;
+//         };
 
-        service.save = function (path, name, contents) {
-          var deferred = $q.defer();
+//         service.save = function (path, name, contents) {
+//           var deferred = $q.defer();
 
-          // Your magic goes here:
-           // Do deferred.resolve(data); to fulfull the promise or
-           // deferred.reject(error); to reject it.
+//           // Your magic goes here:
+//            // Do deferred.resolve(data); to fulfull the promise or
+//            // deferred.reject(error); to reject it.
 
-           return deferred.promise;
-        };
+//            return deferred.promise;
+//         };
 
-        return service;
-      })
-      .run(function (MyFileSystem, config, eventService) {
-        // Set MyFileSystem as the filesystem to use
-        config.set('fsFactory', 'MyFileSystem');
+//         return service;
+//       })
+//       .run(function (MyFileSystem, config, eventService) {
+//         // Set MyFileSystem as the filesystem to use
+//         config.set('fsFactory', 'MyFileSystem');
 
-        // In case you want to send notifications to the user
-        // (for instance, that he must login to save).
-        // The expires flags means whether
-        // it should be hidden after a period of time or the
-        // user should dismiss it manually.
-        eventService.broadcast('event:notification',
-          {message: 'File saved.', expires: true});
+//         // In case you want to send notifications to the user
+//         // (for instance, that he must login to save).
+//         // The expires flags means whether
+//         // it should be hidden after a period of time or the
+//         // user should dismiss it manually.
+//         eventService.broadcast('event:notification',
+//           {message: 'File saved.', expires: true});
 
-      });
+//       });
+
+// angular.bootstrap(document.getElementById('ramlEditor'), ['ramlEditorApp']);
+
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,14 +3,15 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('ConverterSource', ['$scope', function($scope) {
     $scope.editorOptions = {
-        lineWrapping : true,
-        lineNumbers: true,
-        // readOnly: 'nocursor',
-        mode: 'raml',
+      lineWrapping : true,
+      lineNumbers: true,
+      // readOnly: 'nocursor',
+      mode: 'raml',
     };
   }])
+  .controller('MyCtrl1', ['$scope', function($scope) {
+  }])
   .controller('MyCtrl2', ['$scope', function($scope) {
-
   }]);
